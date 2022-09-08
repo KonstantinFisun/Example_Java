8e895ae39b9977c4630e61c63cf06793a6718c4a
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-09-08 19:31:56 +0300

    Задание 2

--- src/Task_2.java
+++ src/Task_2.java
@@ -6,21 +6,39 @@ public class Task_2 {
         int myrank = MPI.COMM_WORLD.Rank();
         int size = MPI.COMM_WORLD.Size();
         int tag = 100;
+        int s;
+        int buf;
 
+        if(myrank == 0){
+            buf = myrank;
+            System.out.println("This is process = "+ myrank+ " " + "Sending : " + buf);
+            int[] numbertosend = new int[1];
+            numbertosend[0] = buf;
+            MPI.COMM_WORLD.Send(numbertosend,0,  1, MPI.INT, myrank+1, tag);
+        } else if (myrank + 1 == size) {
+            buf = myrank;
+            int[] numbertorecv = new int[1];
+            MPI.COMM_WORLD.Recv(numbertorecv, 0, 1, MPI.INT, myrank-1, tag);
+            System.out.println("This is process = "+ myrank+ " " + "   Receive from = "+ (myrank-1) + " Receive: " + numbertorecv[0] + " Sending : " + buf);
 
-        if(myrank % 2 == 0){
-            if(myrank + 1 != size){
-                int[] numbertosend = new int[1];
-                numbertosend[0] = 100+myrank;
-                System.out.println("This is process = "+myrank+"" + "    Sending : "  + numbertosend[0]);
-                MPI.COMM_WORLD.Send(numbertosend,0,  1, MPI.INT, myrank+1, tag);
-            }
+            s = numbertorecv[0] + buf;
+
+            System.out.println("Sum : " + s);
         }
         else{
+            buf = myrank;
             int[] numbertorecv = new int[1];
-            MPI.COMM_WORLD.Recv(numbertorecv, 0, 1, MPI.INT, MPI.ANY_SOURCE, tag);
-            System.out.println("This is process = "+ myrank+ " " + "   Receive from = "+ (myrank-1) + "    Receive: " + numbertorecv[0]);
+            MPI.COMM_WORLD.Recv(numbertorecv, 0, 1, MPI.INT, myrank-1, tag);
+            System.out.println("This is process = "+ myrank+ " " + "   Receive from = "+ (myrank-1) + " Receive: " + numbertorecv[0] + " Sending : " + buf);
+
+            s = numbertorecv[0] + buf;
+
+            int[] numbertosend = new int[1];
+            numbertosend[0] = buf;
+            MPI.COMM_WORLD.Send(numbertosend,0,  1, MPI.INT, myrank+1, tag);
         }
+
+
 
         MPI.Finalize();
     }
